deb5cff2a7f0656aebfb68c81e31c64f51a225db
john01dav <john01dav@gmail.com>
2019-12-22 03:09:54 -0600

    Added php

---
+++ back-end/get_pings.php
@@ -0,0 +1,40 @@
+<?php
+  ob_start();
+  require('config.php');
+
+  if(!isset($_GET['start_date']) || !isset($_GET['end_date'])){
+    echo 'You must specify start_date and end_date via GET parameters.';
+    exit;
+  }
+
+  header('Content-Type: application/json');
+
+  echo "[\n";
+  $statement = $db->prepare('SELECT pings.serverId,pings.pingTime,pings.playersOnline,pings.playersMax,server.serverName FROM pings INNER JOIN server ON server.serverId=pings.serverId; WHERE pings.pingTime<=:end AND pings.pingTime>=:start');
+  $statement->bindParam('end', $_GET['end_date']);
+  $statement->bindParam('start', $_GET['start_date']);
+  $statement->execute();
+  while($row = $statement->fetch()){
+    $ping_time = $row['pingTime'];
+    $server_name = $row['serverName'];
+    $server_id = $row['serverId'];
+    $online_players = $row['playersOnline'];
+    $max_players = $row['playersMax'];
+    echo "    {
+      \"pingTime\": \"$ping_time\",
+      \"server\": {
+        \"name\": \"$server_name\",
+        \"id\": $server_id
+      },
+      \"players\": {
+        \"online\": $online_players,
+        \"max\": $max_players
+      }
+    },\n";
+  }
+  $buffer = ob_get_clean();
+  $buffer2 = substr($buffer, 0, strlen($buffer)-2);
+  echo $buffer2;
+  echo "\n";
+  echo "]";
+?>
